aa347ba6edb94a7b2f33ac8cbb020f56577d0b35
xschen <xs0040@gmail.com>
2018-04-11 15:28:02 +0800

    fix issue with the predict method

--- mxnet_audio/library/cifar10.py
+++ mxnet_audio/library/cifar10.py
@@ -249,8 +249,8 @@ class Cifar10AudioClassifier(object):
 
     def predict(self, audio_path):
         mg = compute_melgram(audio_path)
-        mg = np.expand_dims(mg, axis=0)
-        return self.model(mg)[0].asnumpy()
+        mg = nd.array(np.expand_dims(mg, axis=0), ctx=self.model_ctx)
+        return self.model(mg).asnumpy()[0]
 
     def predict_class(self, audio_path):
         predicted = self.predict(audio_path)
--- mxnet_audio/library/resnet_v2.py
+++ mxnet_audio/library/resnet_v2.py
@@ -274,8 +274,8 @@ class ResNetV2AudioClassifier(object):
 
     def predict(self, audio_path):
         mg = compute_melgram(audio_path)
-        mg = np.expand_dims(mg, axis=0)
-        return self.model(mg)[0].asnumpy()
+        mg = nd.array(np.expand_dims(mg, axis=0), ctx=self.model_ctx)
+        return self.model(mg).asnumpy()[0]
 
     def predict_class(self, audio_path):
         predicted = self.predict(audio_path)
